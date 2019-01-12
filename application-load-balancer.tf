1aaea89c28a90dfe86ec1b44a080827cd0188c1e
Chris Howe-Jones <chris.howejones@gmail.com>
2019-01-12 17:12:45 +0000

    Rename alb

--- application-load-balancer.tf
+++ application-load-balancer.tf
@@ -1,15 +1,15 @@
-resource "aws_alb" "ecs-load-balancer" {
-  name                = "ecs-load-balancer"
+resource "aws_alb" "film-ratings-alb-load-balancer" {
+  name                = "film-ratings-alb-load-balancer"
   security_groups     = ["${aws_security_group.film_ratings_public_sg.id}"]
   subnets             = ["${aws_subnet.film_ratings_public_sn_01.id}", "${aws_subnet.film_ratings_public_sn_02.id}"]
 
   tags {
-    Name = "ecs-load-balancer"
+    Name = "film-ratings-alb-load-balancer"
   }
 }
 
-resource "aws_alb_target_group" "ecs-target-group" {
-  name                = "ecs-target-group"
+resource "aws_alb_target_group" "film-ratings-app-target-group" {
+  name                = "film-ratings-app-target-group"
   port                = "3000"
   protocol            = "HTTP"
   vpc_id              = "${aws_vpc.film_ratings_vpc.id}"
@@ -30,17 +30,17 @@ resource "aws_alb_target_group" "ecs-target-group" {
   }
 
   tags {
-    Name = "ecs-target-group"
+    Name = "film-ratings-app-target-group"
   }
 }
 
 resource "aws_alb_listener" "alb-listener" {
-  load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+  load_balancer_arn = "${aws_alb.film-ratings-alb-load-balancer.arn}"
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+    target_group_arn = "${aws_alb_target_group.film-ratings-app-target-group.arn}"
     type             = "forward"
   }
 }
