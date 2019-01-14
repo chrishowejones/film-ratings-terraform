aad282e94d35ea7e1652a5f48235a385b5f1834a
Chris Howe-Jones <chris.howejones@gmail.com>
2019-01-14 12:57:59 +0000

    Updated depends on.

--- film-ratings-app-service.tf
+++ film-ratings-app-service.tf
@@ -1,14 +1,14 @@
 resource "aws_ecs_service" "film_ratings_app_service" {
-  	name            = "film_ratings_app_service"
-  	iam_role        = "${aws_iam_role.ecs-service-role.name}"
-  	cluster         = "${aws_ecs_cluster.film_ratings_ecs_cluster.id}"
+  name            = "film_ratings_app_service"
+  iam_role        = "${aws_iam_role.ecs-service-role.name}"
+  cluster         = "${aws_ecs_cluster.film_ratings_ecs_cluster.id}"
   task_definition = "${aws_ecs_task_definition.film_ratings_app.family}:${max("${aws_ecs_task_definition.film_ratings_app.revision}", "${data.aws_ecs_task_definition.film_ratings_app.revision}")}"
-        depends_on      = [ "aws_ecs_service.film_ratings_db_service"]
-  	desired_count   = 1
+  depends_on      = [ "aws_ecs_service.film_ratings_db_service"]
+  desired_count   = 1
 
-  	load_balancer {
-    	target_group_arn  = "${aws_alb_target_group.film_ratings_app_target_group.arn}"
-    	container_port    = 3000
-    	container_name    = "film_ratings_app"
-	}
+  load_balancer {
+    target_group_arn  = "${aws_alb_target_group.film_ratings_app_target_group.arn}"
+    container_port    = 3000
+    container_name    = "film_ratings_app"
+  }
 }
